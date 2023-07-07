d28fa08f9e6905da80e4a67956c955c079cbddb9
Guilherme Salustiano <guissalustiano@gmail.com>
2023-07-07 09:07:12 -0300

    update binary search

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -4,7 +4,7 @@ use std::cmp::Ordering;
 pub fn search(nums: Vec<i32>, target: i32) -> i32 {
     let (mut low, mut high) = (0, nums.len());
     while low < high {
-        let mid = low + (high - low) / 2;
+        let mid = (high + low) / 2;
         match nums[mid].cmp(&target) {
             Ordering::Equal => return mid as i32,
             Ordering::Less => low = mid + 1,
